9277b2aca4809e69c9f2ad733073c51ac12f9485
Karee <thisiskaree@gmail.com>
2026-05-03 01:31:55 +0530

    Question_4_a.cpp

--- Lab - 5/Question_4_a.cpp
+++ Lab - 5/Question_4_a.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
 using namespace std;
-
 class A {
 public:
     void showA() {
         cout << "Class A" << endl;
     }
 };
-
 class B : public A {
 public:
     void showB() {
         cout << "Class B" << endl;
     }
 };
-
 int main() {
     B obj;
     obj.showA();
