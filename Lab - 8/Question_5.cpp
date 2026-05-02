29ba096cfa16787d1be4153ab7fda4f7aa0aa1c4
Karee <thisiskaree@gmail.com>
2026-05-03 01:49:18 +0530

    Question_5.cpp

--- Lab - 8/Question_5.cpp
+++ Lab - 8/Question_5.cpp
@@ -4,17 +4,14 @@ template <class T>
 void process(T a) {
     cout << "Single parameter: " << a << endl;
 }
-
 template <class T>
 void process(T a, T b) {
     cout << "Two parameters (same type): " << a << " , " << b << endl;
 }
-
 template <class T, class U>
 void process(T a, U b) {
     cout << "Two parameters (different types): " << a << " , " << b << endl;
 }
-
 int main() {
     process(10);             
     process(5, 15);          
