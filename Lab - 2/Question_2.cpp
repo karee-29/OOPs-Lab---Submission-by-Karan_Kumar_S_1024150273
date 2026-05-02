1b32ba4ed1110185f624d40391d46206aac91635
Karee <thisiskaree@gmail.com>
2026-05-03 01:21:44 +0530

    Question_2.cpp

--- Lab - 2/Question_2.cpp
+++ Lab - 2/Question_2.cpp
@@ -1,55 +1,45 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
 class Student
 {
-private: // only accessible inside the class
+private: 
     string name;
     int rollNo;
     string degree;
     string hostel;
     float cgpa;
-
-    void updateDetails() // private function
+    void updateDetails() 
     {
         cout << "Update name: ";
         cin >> name;
         cout << "Update degree: ";
         cin >> degree;
     }
-
-public: // accessible outside the class
+public:
     void addDetails()
     {
         cout << "Enter name: ";
         cin >> name;
-
         cout << "Enter roll number: ";
         cin >> rollNo;
-
         cout << "Enter degree: ";
         cin >> degree;
-
         cout << "Enter hostel name: ";
         cin >> hostel;
-
         cout << "Enter CGPA: ";
         cin >> cgpa;
     }
-
     void updateCGPA()
     {
         cout << "Enter new CGPA: ";
         cin >> cgpa;
     }
-
     void updateHostel()
     {
         cout << "Enter new hostel name: ";
         cin >> hostel;
     }
-
     void displayDetails()
     {
         cout << "\nStudent Details";
@@ -60,18 +50,13 @@ public: // accessible outside the class
         cout << "\nCGPA: " << cgpa;
     }
 };
-
 int main()
 {
     Student s;
-
     s.addDetails();
     s.displayDetails();
-
-    s.updateCGPA();   // public
-    s.updateHostel(); // public
-
+    s.updateCGPA();   
+    s.updateHostel(); 
     s.displayDetails();
-
     return 0;
 }
