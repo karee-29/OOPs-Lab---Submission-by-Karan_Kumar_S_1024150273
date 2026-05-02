e94815a5e33f33f2c0483e8e80b989eb78509d97
Karee <thisiskaree@gmail.com>
2026-05-03 01:23:49 +0530

    Question_5.cpp

--- Lab - 2/Question_5.cpp
+++ Lab - 2/Question_5.cpp
@@ -1,25 +1,20 @@
 #include <iostream>
 using namespace std;
-
 class Complex
 {
 private:
     float real;
     float imaginary;
-
 public:
     void setComplex(float r, float i)
     {
         real = r;
         imaginary = i;
     }
-
     void displayComplex()
     {
         cout << real << " + " << imaginary << "i" << endl;
     }
-
-    // Function to add two Complex numbers (pass objects and return object)
     Complex addComplex(Complex c)
     {
         Complex temp;
@@ -28,36 +23,28 @@ public:
         return temp;
     }
 };
-
 int main()
 {
     Complex c1, c2, sum;
     float r, i;
-
     cout << "Enter first complex number:" << endl;
     cout << "Real part: ";
     cin >> r;
     cout << "Imaginary part: ";
     cin >> i;
     c1.setComplex(r, i);
-
     cout << "Enter second complex number:" << endl;
     cout << "Real part: ";
     cin >> r;
     cout << "Imaginary part: ";
     cin >> i;
     c2.setComplex(r, i);
-
     cout << "\nFirst complex number: ";
     c1.displayComplex();
-
     cout << "Second complex number: ";
     c2.displayComplex();
-
-    // Add two complex numbers
     sum = c1.addComplex(c2);
     cout << "\nSum of complex numbers: ";
     sum.displayComplex();
-
     return 0;
 }
