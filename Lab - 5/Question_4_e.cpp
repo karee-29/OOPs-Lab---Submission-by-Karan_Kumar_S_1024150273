5b0308c24e4b812d46e44499ef92f7b8ce5f0e0e
Karee <thisiskaree@gmail.com>
2026-05-03 01:32:53 +0530

    Question_4_e.cpp

--- Lab - 5/Question_4_e.cpp
+++ Lab - 5/Question_4_e.cpp
@@ -1,34 +1,29 @@
 #include <iostream>
 using namespace std;
-
 class A {
 public:
     void showA() {
         cout << "Class A" << endl;
     }
 };
-
 class B : public A {
 public:
     void showB() {
         cout << "Class B" << endl;
     }
 };
-
 class C : public A {
 public:
     void showC() {
         cout << "Class C" << endl;
     }
 };
-
 class D : public B, public C {
 public:
     void showD() {
         cout << "Class D" << endl;
     }
 };
-
 int main() {
     D obj;
     obj.showB();
